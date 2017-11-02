868abca3d9197fbb2567df067d98ed85e5fcbf6b
Your-Full-NameJohann <jSchuster>
2017-11-01 18:52:06 -0700

    added/404/route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch, Router } from 'react-router-dom'
 import MainPage from './components/mainPage'
 import Post from './components/posts'
 import AddPost from './components/addPost'
 import PostInfo from './components/postInfo'
-//import NotFound from './components/notFound'
+import NotFound from './components/notFound'
 
 
 
@@ -14,12 +14,15 @@ class App extends Component {
   render() {
     
     return (
-      <div>
+   
+      <Switch>
          <Route exact path="/" render={() => (<MainPage/>)}/>
          <Route exact path="/:category" render={() => (<Post/>)}/>      
-         <Route exact path="/posts/:id" render={() => (<PostInfo/>)}/>
-      </div>
-       
+         <Route exact path="/posts/:id" render={() => (<PostInfo/>)}/> 
+         <Route exact path="/new/post" render={() => (<AddPost/>)}/> 
+         <Route exact path="*" render={() => (<NotFound/>)}/>     
+      </Switch> 
+             
     );
   }
 }
